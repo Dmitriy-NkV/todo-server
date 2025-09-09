e046e03b2e58da62398370c82d91d61112a1290c
Dmitriy <Dmitry5335@yandex.ru>
2025-09-09 18:30:15 +0300

    [FEAT] Add Session class declaration

--- include/server.hpp
+++ include/server.hpp
@@ -16,6 +16,25 @@ using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
 
 namespace server
 {
+  class Session: public std::enable_shared_from_this< Session >
+  {
+  public:
+    Session(tcp::socket&& socket, database::Database& db);
+
+    void run();
+    void do_read();
+    void on_read(beast::error_code ec, std::size_t bytes_transferred);
+    void send_response(http::message_generator&& msg);
+    void on_write(bool keep_alive, beast::error_code ec, std::size_t bytes_transferred);
+    void do_close();
+
+  private:
+    beast::tcp_stream stream_;
+    beast::flat_buffer buffer_;
+    http::request< http::string_body > req_;
+    database::Database& db_;
+  };
+
   class Listener: public std::enable_shared_from_this< Listener >
   {
   public:
