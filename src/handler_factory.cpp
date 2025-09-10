ab8c8d8504fa374155f245b9137f86d7192f8918
Dmitriy <Dmitry5335@yandex.ru>
2025-09-11 00:24:49 +0300

    [FEAT] Add handlers to HandlerFactory

--- src/handler_factory.cpp
+++ src/handler_factory.cpp
@@ -1,7 +1,18 @@
 #include "handler_factory.hpp"
+#include "delete_task_handler.hpp"
+#include "get_task_handler.hpp"
+#include "get_tasks_handler.hpp"
+#include "post_task_handler.hpp"
+#include "put_task_handler.hpp"
 
 handlers::HandlerFactory::HandlerFactory():
-  handlers_({})
+  handlers_({
+    std::make_unique< handlers::DeleteTaskHandler >(),
+    std::make_unique< handlers::GetTaskHandler >(),
+    std::make_unique< handlers::GetTasksHandler >(),
+    std::make_unique< handlers::PostTaskHandler >(),
+    std::make_unique< handlers::PutTaskHandler >()
+  })
 {}
 
 std::unique_ptr< handlers::RequestHandler > handlers::HandlerFactory::create_handler(const http::request< http::string_body >& req) const
