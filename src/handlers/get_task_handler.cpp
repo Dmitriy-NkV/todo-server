d9326f7ad4d4be30a5406ae83c9f6c25e49bd817
Dmitriy <Dmitry5335@yandex.ru>
2025-09-12 01:06:52 +0300

    [FEAT] Add handle_request in get task handler

--- src/handlers/get_task_handler.cpp
+++ src/handlers/get_task_handler.cpp
@@ -1,8 +1,49 @@
 #include "get_task_handler.hpp"
+#include "http_utils.hpp"
 
 bool handlers::GetTaskHandler::can_handle(const http::request< http::string_body >& req) const
 {
   return req.method() == http::verb::get && req.target().starts_with("/task");
+}
+
+http::response< http::string_body > handlers::GetTaskHandler::handle_request(const http::request< http::string_body >& req,
+  std::shared_ptr< database::Database >(db))
+{
+  std::vector< std::string > params = utils::parse_parameters(req.target());
+
+  int id = 0;
+  try
+  {
+    if (params.size() == 2)
+    {
+      id = std::stoi(params[1]);
+    }
+    else
+    {
+      throw std::runtime_error("Wrong number of parameters");
+    }
+  }
+  catch (const std::invalid_argument& e)
+  {
+    return utils::create_error_response(http::status::bad_request, "Wrong id");
+  }
+  catch (const std::exception& e)
+  {
+    return utils::create_error_response(http::status::bad_request, e.what());
+  }
+
+  database::Task task(id);
+  nlohmann::json json;
+  try
+  {
+    json = db->get_task_by_id(task);
+  }
+  catch (const std::exception& e)
+  {
+    return utils::create_error_response(http::status::internal_server_error, e.what());
+  }
+
+  return utils::create_json_response(http::status::ok, json);
 }
 
 std::unique_ptr< handlers::RequestHandler > handlers::GetTaskHandler::create() const
