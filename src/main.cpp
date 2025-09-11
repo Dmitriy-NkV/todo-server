3235e9e1c7bf490a77a8f0562ae12610c3662473
Dmitriy <Dmitry5335@yandex.ru>
2025-09-12 01:49:00 +0300

    [TEST] Add main for tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,16 @@
+#include "server.hpp"
+#include <iostream>
+
 int main()
-{}
+{
+  auto db = std::make_shared< database::Database >("host=localhost dbname=todoapp user=postgres password=admin");
+  server::Server server("127.0.0.1", 8080, 1, db);
+
+  server.start();
+  while (std::cin)
+  {
+    continue;
+  }
+
+  return 0;
+}
