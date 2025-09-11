7bf51b9d27f2309c5099679a73a9ec89bb21282f
Dmitriy <Dmitry5335@yandex.ru>
2025-09-12 01:51:26 +0300

    [TEST] Add try catch in main

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,20 @@
 
 int main()
 {
-  auto db = std::make_shared< database::Database >("host=localhost dbname=todoapp user=postgres password=admin");
-  server::Server server("127.0.0.1", 8080, 1, db);
-
-  server.start();
-  while (std::cin)
+  try
   {
-    continue;
+    auto db = std::make_shared< database::Database >("host=localhost dbname=todoapp user=postgres password=admin");
+    server::Server server("127.0.0.1", 8080, 1, db);
+    server.start();
+    while (std::cin)
+    {
+      continue;
+    }
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << e.what() << '\n';
+    return 1;
   }
 
   return 0;
