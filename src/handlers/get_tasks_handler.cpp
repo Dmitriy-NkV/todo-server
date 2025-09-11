a01dd7642b2e480e729dc351d9b6e51a88b157b0
Dmitriy <Dmitry5335@yandex.ru>
2025-09-12 01:07:18 +0300

    [FEAT] Add handle_request in get tasks handler

--- src/handlers/get_tasks_handler.cpp
+++ src/handlers/get_tasks_handler.cpp
@@ -1,8 +1,32 @@
 #include "get_tasks_handler.hpp"
+#include "http_utils.hpp"
 
 bool handlers::GetTasksHandler::can_handle(const http::request< http::string_body >& req) const
 {
   return req.method() == http::verb::get && req.target().starts_with("/tasks");
+}
+
+http::response< http::string_body > handlers::GetTasksHandler::handle_request(const http::request< http::string_body >& req,
+  std::shared_ptr< database::Database > db)
+{
+  std::vector< std::string > params = utils::parse_parameters(req.target());
+
+  if (params.size() != 1)
+  {
+    return utils::create_error_response(http::status::bad_request, "Wrong parameters");
+  }
+
+  nlohmann::json json;
+  try
+  {
+    json = db->get_all_tasks();
+  }
+  catch (const std::exception& e)
+  {
+    return utils::create_error_response(http::status::internal_server_error, e.what());
+  }
+
+  return utils::create_json_response(http::status::ok, json);
 }
 
 std::unique_ptr< handlers::RequestHandler > handlers::GetTasksHandler::create() const
