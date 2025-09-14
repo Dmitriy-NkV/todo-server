e0d21d8bb35b9046ed51f7559e7f598a7de064d8
Dmitriy <Dmitry5335@yandex.ru>
2025-09-14 21:00:05 +0300

    [FEAT] Add declaration of Logger class

---
+++ include/logger.hpp
@@ -0,0 +1,34 @@
+#ifndef LOGGER_HPP
+#define LOGGER_HPP
+
+#include <string>
+#include <iostream>
+#include <format>
+#include <chrono>
+
+namespace logger
+{
+  enum class LogLevel
+  {
+    DEBUG,
+    INFO,
+    WARNING,
+    ERROR,
+    CRITICAL
+  };
+
+  class Logger
+  {
+  public:
+    Logger() = default;
+    ~Logger() = default;
+
+    static Logger& get_instance();
+    void log(LogLevel level, const std::string& message);
+
+  private:
+    std::string level_to_string(LogLevel level);
+  };
+}
+
+#endif
