3ab04df03cf1ef7111dbf616efe69ac0d6e7664c
Dmitriy <Dmitry5335@yandex.ru>
2025-09-14 20:03:34 +0300

    [FEAT] Add new enviroment variables to main

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,9 @@ int main()
     std::string db_name = std::getenv("DB_NAME") ? std::getenv("DB_NAME") : "todoapp";
     std::string db_user = std::getenv("DB_USER") ? std::getenv("DB_USER") : "postgres";
     std::string db_password = std::getenv("DB_PASSWORD") ? std::getenv("DB_PASSWORD") : "admin";
+    std::string server_host = std::getenv("SERVER_HOST") ? std::getenv("SERVER_HOST") : "0.0.0.0";
+    unsigned short server_port = std::getenv("SERVER_PORT") ? std::atoi(std::getenv("SERVER_PORT")) : 9000;
+    size_t threads_num = std::getenv("THREADS_NUM") ? std::stoull(std::getenv("THREADS_NUM")) : 1;
 
     std::string connection_string = "host=" + db_host +
       " port=" + db_port +
@@ -22,16 +25,21 @@ int main()
 
     db->initialize_database();
 
-    server::Server server("0.0.0.0", 8000, 1, db);
+    server::Server server(server_host, server_port, threads_num, db);
     server.start();
+
+    std::cout << "Server started" << '\n';
+
     std::string line;
     while (std::getline(std::cin, line))
     {
-      if (line == "exit")
+      if (line == "stop")
       {
         break;
       }
     }
+
+    std::cout << "Server stoped" << '\n';
 
     server.stop();
   }
