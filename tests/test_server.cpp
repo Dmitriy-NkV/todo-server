73a44f25bd3156175c07725959326f90cc5d3aed
Dmitriy <Dmitry5335@yandex.ru>
2025-09-18 19:05:16 +0300

    [TEST] Add Delete task test

--- tests/test_server.cpp
+++ tests/test_server.cpp
@@ -78,7 +78,7 @@ namespace tests
     };
 
     http::response< http::string_body > update_response;
-    ASSERT_NO_THROW(update_response = client.request(http::verb::post, "/task", create_json));
+    ASSERT_NO_THROW(update_response = client.request(http::verb::put, "/task", create_json));
 
     ASSERT_EQ(update_response.result(), http::status::accepted);
     ASSERT_EQ(update_response[http::field::content_type], "application/json");
@@ -92,5 +92,42 @@ namespace tests
     auto get_json_response = nlohmann::json::parse(get_response.body());
     EXPECT_EQ(create_json_response["id"].get< int >(), task_id);
     EXPECT_EQ(create_json_response["title"].get< std::string >(), "New title");
+  }
+
+  TEST_F(TestServerFixture, DeleteTask)
+  {
+    HttpClient client("127.0.0.1", 9000);
+
+    nlohmann::json create_json = {
+      { "title", "Title" },
+      { "description", "Description" },
+      { "status", "Todo" }
+    };
+
+    http::response< http::string_body > create_response;
+    ASSERT_NO_THROW(create_response = client.request(http::verb::post, "/task", create_json));
+
+    ASSERT_EQ(create_response.result(), http::status::created);
+    ASSERT_EQ(create_response[http::field::content_type], "application/json");
+
+    auto create_json_response = nlohmann::json::parse(create_response.body());
+    ASSERT_TRUE(create_json_response.contains("message") && !create_json_response["message"].is_null());
+
+    int task_id = create_json_response["message"].get< int >();
+
+    http::response< http::string_body > delete_response;
+    ASSERT_NO_THROW(delete_response = client.request(http::verb::delete_, "/task" + std::to_string(task_id)));
+
+    ASSERT_EQ(delete_response.result(), http::status::accepted);
+    ASSERT_EQ(delete_response[http::field::content_type], "application/json");
+
+    http::response< http::string_body > get_response;
+    ASSERT_NO_THROW(get_response = client.request(http::verb::get, "/task" + std::to_string(task_id)));
+
+    ASSERT_EQ(get_response.result(), http::status::ok);
+    ASSERT_EQ(get_response[http::field::content_type], "application/json");
+
+    auto get_json_response = nlohmann::json::parse(get_response.body());
+    EXPECT_EQ(get_json_response["message"].get< std::string >(), "No task with current id");
   }
 }
